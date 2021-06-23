f79ec0de636af964fb1a73d563c70a997a1640e8
sfp_BrainFry <49707987+syedfairozpasha@users.noreply.github.com>
2021-06-23 23:34:56 +0530

    Reference

---
+++ ReferenceAndPointers.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+void funcwithrefargs(int refparam[],int size)
+{
+  for (int ivar = 0; ivar<size;ivar++)
+  {
+     refparam[ivar] = refparam[ivar]+1;
+    // refparam++;
+  }
+}
+
+void funcwithrefargs2(int (&refparam)[4],int size)
+{
+  for (int ivar = 0; ivar<size;ivar++)
+  {
+     refparam[ivar] = refparam[ivar]+1;
+    // refparam++;
+  }
+}
+
+void funcwithrefargs3(int *refparam,int size)
+{
+  for (int ivar = 0; ivar<size;ivar++)
+  {
+     refparam[ivar] = refparam[ivar]+1;
+    // refparam++;
+  }
+}
+int main()
+{
+  int arr[4] = {1,2,3,4};
+  cout<<"Before\t"<<arr[0]<<endl;
+  funcwithrefargs(arr,4);
+  cout<<"after \t"<<arr[0]<<endl;
+  cout<<"Before\t"<<arr[0]<<endl;
+  funcwithrefargs2(arr,4);
+  cout<<"after \t"<<arr[0]<<endl;
+  cout<<"Before\t"<<arr[0]<<endl;
+  funcwithrefargs3(arr,4);
+  cout<<"after \t"<<arr[0]<<endl;
+}
